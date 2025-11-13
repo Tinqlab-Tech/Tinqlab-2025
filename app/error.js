75713709fa12e1ebe09c567a09d1af724703d763
BANKS <bankoles713@gmail.com>
2025-11-13 09:56:43 +0100

    fixed error

--- app/error.js
+++ app/error.js
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import { MainButton } from "@/components/button/MainButton";
-import PageStarter from "@/components/section/PageStarter";
-import Link from "next/link";
-import PageBorders from "@/components/wrappers/PageBorders";
-import MainText from "@/components/typography/MainText";
-import HeaderOne from "@/components/typography/HeaderOne";
+import { MainButton } from '@/components/button/MainButton';
+import PageStarter from '@/components/section/PageStarter';
+import Link from 'next/link';
+import PageBorders from '@/components/wrappers/PageBorders';
+import MainText from '@/components/typography/MainText';
+import HeaderOne from '@/components/typography/HeaderOne';
 
 export default function Page() {
   return (
     <div className="min-h-screen">
       <PageBorders>
-        <div className="flex flex-col justify-center items-center text-center gap-4 ">
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
           <HeaderOne
-            text={"Something Went Wrong"}
-            color={"text-[#131313]"}
+            text={'Something Went Wrong'}
+            color={'text-[#131313]'}
             size={
-              "text-[24px] lg:text-[32px] leading-[30px] lg:leading-[40px] "
+              'text-[24px] lg:text-[32px] leading-[30px] lg:leading-[40px] '
             }
           />
           <MainText
@@ -24,7 +24,7 @@ export default function Page() {
             text="We have been notified. Please refresh the page after a few minutes or try again later. We are working on it."
           />
           <Link href="/">
-            <MainButton text={"Go Home"} />
+            <MainButton text={'Go Home'} />
           </Link>
         </div>
       </PageBorders>
--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,29 @@
-"use client";
+'use client';
 
-import "./globals.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import React from "react";
-import Footer from "@/components/partials/Footer";
-import Header from "@/components/partials/Header";
-import Script from "next/script";
-import { Lato } from "next/font/google";
-import HomepageHero from "@/components/hero/HomepageHero";
+import './globals.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import React from 'react';
+import Footer from '@/components/partials/Footer';
+import Header from '@/components/partials/Header';
+import Script from 'next/script';
+import { Lato } from 'next/font/google';
+import HomepageHero from '@/components/hero/HomepageHero';
 
 //fonts
 const lato = Lato({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"], // choose the weights you need
-  style: ["normal", "italic"], // optional
-  variable: "--font-lato", // optional for CSS variable usage
-  display: "swap", // improves font loading
+  subsets: ['latin'],
+  weight: ['300', '400', '700'], // choose the weights you need
+  style: ['normal', 'italic'], // optional
+  variable: '--font-lato', // optional for CSS variable usage
+  display: 'swap', // improves font loading
 });
 export default function RootLayout({ children }) {
   const [isOpen, setIsOpen] = React.useState(true);
 
   return (
     <>
-      <html lang="en" className={`${lato.className} font-sans z-10`}>
+      <html lang="en" className={`${lato.className} z-10 font-sans`}>
         <body className="tracking-tight">
           <main>
             {children}
